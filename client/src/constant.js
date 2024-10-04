145ca6a85c9cd04ef675ab0bb9f0fe6c21c6140e
Jack1998 <yaduvanshiabhi1998@gmail.com>
2024-10-04 22:10:48 +0530

    Refactor SERVER_URL to dynamically set the protocol and hostname

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,1 +1,3 @@
-export const SERVER_URL = "http://localhost:3000/api/v1";
+// export const SERVER_URL = "http://localhost:10000/api/v1";
+
+export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:3000/api/v1`;

fb7fc3db2060c3f0b76d011910b9265e2b22f9c6
Robin <goyalrobin12@gmail.com>
2024-10-04 11:59:08 +0530

    remove the db dtring

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
-    // const MONGO_URI = process.env["MONGO_URI"];
-    const MONGO_URI =
-      "mongodb+srv://j90811981:PqesdcabZAaaxTLE@cluster0.pen5mz8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const MONGO_URI = process.env["MONGO_URI"];
+
     if (MONGO_URI)
       await mongoose.connect(MONGO_URI, { dbName: "visitingCards" });
     if (MONGO_URI) console.log("Connected to DB:", mongoose.connection.name);

e4e39eb79d045017a0a30199d654de75324b01f6
Jack1998 <yaduvanshiabhi1998@gmail.com>
2024-10-05 12:40:13 +0530

    Refactor SERVER_URL in constant.js to remove dynamic port number

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,3 +1,1 @@
-// export const SERVER_URL = "http://localhost:10000/api/v1";
-
-export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:3000/api/v1`;
+export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}/api/v1`;

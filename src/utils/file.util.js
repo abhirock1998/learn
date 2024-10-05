6fbd4a2c646652fbeb63be869687a94eef07b829
Jack1998 <yaduvanshiabhi1998@gmail.com>
2024-10-05 12:49:26 +0530

    Refactor .gitignore to exclude dist folder

--- src/utils/file.util.js
+++ src/utils/file.util.js
@@ -3,13 +3,15 @@ const fs = require("fs");
 
 const uploadFolder = path.join(__dirname, "..", "uploads");
 
+console.log(`Upload folder: ${uploadFolder}`);
+
 const getImagePath = (fileName) => `${uploadFolder}/${fileName}`;
 
 const deleteFile = (fileName) => {
   const filePath = getImagePath(fileName);
   fs.unlink(filePath, (err) => {
     if (err) {
-      console.error(err);
+      console.error(`Error deleting file: ${filePath}`, err.message);
       return;
     }
     console.log(`Deleted file: ${filePath}`);
